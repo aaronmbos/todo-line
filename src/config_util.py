6fc9f09ac993e2d1dd778aa95e1294ee8b8ddfab
aaronmbos <aaronmbos@gmail.com>
2019-12-09 21:23:59 -0600

    Continue to implement config_util.py

--- src/config_util.py
+++ src/config_util.py
@@ -1,4 +1,25 @@
+from pathlib import Path
+from pathlib import os
+
 
 class ConfigUtil:
-    def test_method(self):
-        print("test")
+    _config_dir_name = 'pytodo_config'
+    _config_file_name = 'pytodo.json'
+
+
+    def __init__(self):
+        pass
+
+
+    def get_config_path(self):
+        if os.name == 'posix':
+            return f'{Path.home()}/Applications/Pytodo/configs'
+
+
+    def get_or_set(self):
+        config_path = self.get_config_path()
+        path = Path(config_path)
+        if not path.exists():
+            path.mkdir()
+        else:
+            pass
--- src/todo.py
+++ src/todo.py
@@ -2,4 +2,4 @@ from factory import Factory
 
 if __name__ == "__main__":
     _factory = Factory()
-    _factory.config_util_create().test_method()
+    _factory.config_util_create().get_or_set()
