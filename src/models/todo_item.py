60b3d0cb3afc8dedefbde436a264108cb7910ce6
aaronmbos <aaronmbos@gmail.com>
2019-12-16 21:34:15 -0600

    Add todo item model

---
+++ src/models/todo_item.py
@@ -0,0 +1,5 @@
+class TodoItem:
+
+    def __init__(self):
+        pass
+        
--- src/todo_data.py
+++ src/todo_data.py
@@ -9,6 +9,12 @@ class TodoData:
         self._todoFileLocation = self._config_util.get_config_setting('todoFileLocation')
 
     def create_todo(self, todo_name):
-        new_todo = {'title': todo_name}
+        todos = self.get_all_todos()
+        new_todo = {'title': todo_name, 'todos':[]}
+        todos.append(new_todo)
         with open(self._todoFileLocation, 'w') as file:
-            json.dump(new_todo, file)
+            json.dump(todos, file)
+
+    def get_all_todos(self):
+        with open(self._todoFileLocation) as file:
+            return json.load(file)
