e2b5d210c9b927c0c1cbd9fc99ed39f0aeb61057
aaronmbos <aaronmbos@gmail.com>
2020-02-03 21:43:47 -0600

    Update todo item print formatting

--- src/todo_processor.py
+++ src/todo_processor.py
@@ -48,7 +48,7 @@ class TodoProcessor:
         todo = self._todo_data.get_active_todo()
         formatted_items = f'\nItems in {todo["title"]}:\n'
         for idx, item in enumerate(todo['items']):
-            formatted_items += f'+---+\n| {"x" if item["status"] == "completed" else " "} |  {idx + 1}. {item["desc"]}\n+---+\n'
+            formatted_items += f'[{"x" if item["status"] == "completed" else " "}] {idx + 1}. {item["desc"]}\n'
         print(formatted_items)
 
     def validate_todo(self, todo_name):
