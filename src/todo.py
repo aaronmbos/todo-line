05d18dbfd2c74e83f51fb9a0430428348f7de153
aaronmbos <aaronmbos@gmail.com>
2020-02-04 22:27:58 -0600

    Begin transition from top level args to sub commands

--- src/todo.py
+++ src/todo.py
@@ -4,5 +4,4 @@ from factory import Factory
 if __name__ == "__main__":
     _factory = Factory() 
 
-    parsed_args = _factory.create_arg_parser().parse_args()
-    _factory.create_todo_processor().process_todo(parsed_args)
+    parsed_args = _factory.create_arg_parser().process_todo_request()
